b37d34a61f19697b7012bdb1b668395982d6ae61
Benjamin Morgan <benmorgan2@gmail.com>
2019-08-26 18:28:13 +0100

    Fixed version import in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from site_analysis import __version__ as VERSION
+from py_sc_fermi import __version__ as VERSION
 
 readme = 'README.md'
 long_description = open( readme ).read()
