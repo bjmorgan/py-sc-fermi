eb8d7be3ec2c09a9cc238ed20261cf1cbb4c23e0
Benjamin Morgan <benmorgan2@gmail.com>
2019-08-28 08:34:00 +0100

    Added docstrings and unittests for DOS class

--- py_sc_fermi/dos.py
+++ py_sc_fermi/dos.py
@@ -1,37 +1,64 @@
 import numpy as np
 
 class DOS(object):
+    """Class for handling density-of-states data and its integration"""
 
-    def __init__(self, dos, edos, egap, nelect, normalise=True):
+    def __init__(self, dos, edos, egap, nelect, normalise=True, replicate_sc_fermi=True):
+        """Initialise a DOS instance.
+
+        Args:
+            dos (np.array): Density of states data.
+            edos (np.array): Energies for the density of states (in eV).
+            egap (float): Width of the band gap (in eV).
+            nelect (int): Number of electrons.
+            normalise (:obj:`bool`, optional): Normalise the DOS so that the integral
+                from e_min --> 0.0 is equal to the `nelect`. Default is `True`.
+            replicate_sc_fermi (:obj:`bool`, optional): If True will reproduce off-by-one
+                errors in SC-Fermi when integrating the DOS. Default is `True`.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If `egap` > `max(edos)`.
+
+        """
         if egap > edos[-1]:
-            raise ValueError('ERROR: your conduction band is not present in energy range of DOS!!')
+            raise ValueError('ERROR: Your gap extends beyond your maximum energy')
         self._dos = dos
         self._edos = edos
         self._egap = egap
         self._nelect = nelect
+        self._replicate_sc_fermi = replicate_sc_fermi
         if normalise:
             self.normalise_dos()
 
     @property
     def dos(self):
+        """Get the dos array."""
         return self._dos
 
     @property
     def edos(self):
+        """Get the edos array."""
         return self._edos
 
     @property
     def egap(self):
+        """Get the bandgap."""
         return self._egap
 
     @property
     def nelect(self):
+        """Get the number of electrons in the unit cell."""
         return self._nelect
 
     def normalise_dos(self):
         vbm_index = np.where(self._edos <= 0)[0][-1]
-        sum1 = np.trapz(self._dos[:vbm_index+2], self._edos[:vbm_index+2]) # BJM: possible off-by-one error?
-        # np_edos[vbm_index+2] has a positive energy.
+        if self._replicate_sc_fermi:
+            sum1 = np.trapz(self._dos[:vbm_index+2], self._edos[:vbm_index+2]) # Off-by-one error
+        else:
+            sum1 = np.trapz(self._dos[:vbm_index+1], self._edos[:vbm_index+1]) # Correct integral
         self._dos = self._dos / sum1 * self._nelect
 
     def emin(self):
@@ -44,8 +71,12 @@ class DOS(object):
         # get n0 and p0 using integrals (equations 28.9 in Ashcroft Mermin)
         p0_index = np.where(self._edos <= 0)[0][-1]
         n0_index = np.where(self._edos > self.egap)[0][0]
-        p0 = np.trapz( p_func(e_fermi, self._dos[:p0_index+2], self._edos[:p0_index+2], kT ),
-                       self._edos[:p0_index+2]) # BJM: possible off-by-one error?
+        if self._replicate_sc_fermi:
+            p0 = np.trapz( p_func(e_fermi, self._dos[:p0_index+2], self._edos[:p0_index+2], kT ),
+                           self._edos[:p0_index+2]) # Off-by-one error
+        else:
+            p0 = np.trapz( p_func(e_fermi, self._dos[:p0_index+2], self._edos[:p0_index+1], kT ),
+                           self._edos[:p0_index+1]) # Off-by-one error
         n0 = np.trapz( n_func(e_fermi, self._dos[n0_index:], self._edos[n0_index:], kT ),
                        self._edos[n0_index:])
         return p0, n0
---
+++ tests/test_dos.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest.mock import Mock, patch
+import numpy as np
+from py_sc_fermi.dos import DOS
+
+class TestDOSInit(unittest.TestCase):
+
+    def test_DOS_is_initialised(self):
+        dos_data = np.random.random(100)
+        edos = np.linspace(-10.0, 10.0, 100)
+        egap = 3.0
+        nelect = 10
+        with patch('py_sc_fermi.dos.DOS.normalise_dos') as mock_normalise_dos:
+            dos = DOS( dos=dos_data, edos=edos, egap=egap, nelect=nelect )
+            self.assertEqual( mock_normalise_dos.call_count, 1 )
+        np.testing.assert_equal( dos._dos, dos_data )
+        np.testing.assert_equal( dos._edos, edos )
+        self.assertEqual( dos._egap, egap )
+        self.assertEqual( dos._nelect, nelect )
+
+class TestDos(unittest.TestCase):
+
+    def setUp(self):
+        dos_data = np.ones(101)
+        edos = np.linspace(-10.0, 10.0, 101)
+        egap = 3.0
+        nelect = 10       
+        self.dos = DOS( dos=dos_data, edos=edos, egap=egap, nelect=nelect )
+
+    def test_dos_property(self):
+        np.testing.assert_equal( self.dos.dos, self.dos._dos )
+
+    def test_edos_property(self):
+        np.testing.assert_equal( self.dos.edos, self.dos._edos )
+
+    def test_egap_property(self):
+        self.assertEqual( self.dos.egap, self.dos._egap )
+
+    def test_nelect_property(self):
+        self.assertEqual( self.dos.nelect, self.dos._nelect )
+
+    def test_normalise_dos(self):
+        # The integral of rho(E) = 1, from -10.0 to 0, is equal to 10.0
+        # With nelect = 10.0, the normaliss_dos() method should leave
+        # all dos values = 1.0
+        dos_data = np.ones(101)
+        edos = np.linspace(-10.0, 10.0, 101)
+        egap = 3.0
+        nelect = 10
+        dos = DOS( dos=dos_data, edos=edos, egap=egap, nelect=nelect, replicate_sc_fermi=False )
+        np.testing.assert_equal( dos.dos, dos_data )
+
+if __name__ == '__main__':
+    unittest.main()
