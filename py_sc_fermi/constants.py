b829ccb3ac56b2b6459707db5a45365e9eb682ee
alexsquires <alex.g.squires@gmail.com>
2021-09-10 18:22:59 +0100

    Switch to  scipy constants

--- py_sc_fermi/constants.py
+++ py_sc_fermi/constants.py
@@ -1,3 +1,3 @@
-# from scipy.constants import physical_constants
-# kboltz = physical_constants['Boltzmann constant in eV/K'][0]
-kboltz = 8.617343e-5
+from scipy.constants import physical_constants
+kboltz = physical_constants['Boltzmann constant in eV/K'][0]
+# kboltz = 8.617343e-5
