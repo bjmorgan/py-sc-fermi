61bb1942fa085782ae1b8159755a861a96eac9dc
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:28:58 +0000

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -9,7 +9,7 @@ structure = Structure.from_file(os.path.join(os.path.dirname(__file__)), 'tests/
 volume = Structure.volume
 
 def test_volume_from_structure():
-    assert volume == get_volume_from_structure('inputs/POSCAR')
+    assert volume == get_volume_from_structure(os.path.join(os.path.dirname(__file__)), 'tests/inputs/POSCAR')
 
 # def test_read_unitcell_data():
 #     assert_almost_equal(volume,read_unitcell_data('inputs/unitcell.dat'))
