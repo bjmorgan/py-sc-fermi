c9a758fad13ca77482d88e848b0a44a029e2a20e
alexsquires <alex.g.squires@gmail.com>
2022-02-24 00:42:07 +0000

    Update test_defect_system.py

--- tests/test_defect_system.py
+++ tests/test_defect_system.py
@@ -3,11 +3,23 @@ from unittest.mock import Mock, patch
 
 from py_sc_fermi.defect_system import DefectSystem
 from py_sc_fermi.defect_species import DefectSpecies
+from py_sc_fermi.dos import DOS
 
 class TestDefectSystemInit(unittest.TestCase):
 
     def test_defect_system_is_initialised(self):
-        pass
-       
+        volume = 100
+        mock_defect_species = [ Mock(spec=DefectSpecies), Mock(spec=DefectSpecies) ]
+        dos = Mock(spec=DOS)
+        spin_pol = 1
+        defect_system = DefectSpecies( defect_species=mock_defect_species,
+                                        volume=volume,
+                                        dos=dos,
+                                        spin_pol=spin_pol)
+        self.assertEqual( defect_system._volume, volume )
+        self.assertEqual( defect_system._nsites, nsites )
+        self.assertEqual( defect_system._defect_species[0], mock_defect_species[0] )
+        self.assertEqual( defect_system._defect_species[1], mock_defect_species[1] )
+        
 if __name__ == '__main__':
     unittest.main()
