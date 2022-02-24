754787883ca217930929279fd3f0bc4b03ef3ccf
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:10:37 +0000

    Create test_inputs.py

---
+++ tests/test_inputs.py
@@ -0,0 +1,13 @@
+import unittest
+
+from py_sc_fermi.inputs import volume_from_structure
+from pymatgen.core import Structure
+
+structure = Structure.from_file('../examples/v_F/POSCAR')
+volume = Structure.volume
+
+def test_volume_from_structure():
+    assert volume == get_volume_from_structure('../examples/v_F/POSCAR')
+
+if __name__ == '__main__':
+    unittest.main()
