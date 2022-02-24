7d049386bbaf7c17dcf7b41e75d7ec365d945209
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:35:14 +0000

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -5,11 +5,11 @@ import os
 from py_sc_fermi.inputs import volume_from_structure, read_unitcell_data
 from pymatgen.core import Structure
 
-structure = Structure.from_file(os.path.join(os.path.dirname(__file__)), 'tests/inputs/POSCAR')
+structure = Structure.from_file(os.path.join(os.path.dirname(__file__)), 'tests/inputs/', 'POSCAR')
 volume = Structure.volume
 
 def test_volume_from_structure():
-    assert volume == get_volume_from_structure(os.path.join(os.path.dirname(__file__)), 'tests/inputs/POSCAR')
+    assert volume == get_volume_from_structure(os.path.join(os.path.dirname(__file__)), 'tests/inputs/', 'POSCAR')
 
 # def test_read_unitcell_data():
 #     assert_almost_equal(volume,read_unitcell_data('inputs/unitcell.dat'))
