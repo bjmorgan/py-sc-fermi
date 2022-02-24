deae582accced5dd554d57202f91b8f08f6059e0
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:54:23 +0000

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -5,18 +5,20 @@ import os
 from py_sc_fermi.inputs import volume_from_structure, read_unitcell_data
 from pymatgen.core import Structure
 
-test_data_dir = 'inputs'
-test_poscar_filename = os.path.join( os.path.dirname( __file__ ), test_data_dir, 'POSCAR' )
-test_unitcell_filename = os.path.join( os.path.dirname( __file__ ), test_data_dir, 'unitcell.dat' )
+class TestInputs(unittest.TestCase):
 
-structure = Structure.from_file(test_poscar_filename)
-volume = Structure.volume
+    test_data_dir = 'inputs'
+    test_poscar_filename = os.path.join( os.path.dirname( __file__ ), test_data_dir, 'POSCAR' )
+    test_unitcell_filename = os.path.join( os.path.dirname( __file__ ), test_data_dir, 'unitcell.dat' )
 
-def test_volume_from_structure():
-    assert_almost_equal(volume,volume_from_structure(test_poscar_filename))
+    structure = Structure.from_file(test_poscar_filename)
+    volume = Structure.volume
 
-def test_read_unitcell_data():
-     assert_almost_equal(volume,read_unitcell_data(test_unitcell_filename))
+    def test_volume_from_structure():
+        assert_almost_equal(volume_from_structure(test_poscar_filename),volume)
+
+    def test_read_unitcell_data():
+         assert_almost_equal(read_unitcell_data(test_unitcell_filename),volume)
     
 if __name__ == '__main__':
     unittest.main()
