69004b2472b29f0d7a842f773ce9a58991372e47
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:23:13 +0000

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,16 +1,17 @@
 import unittest
+from numpy.testing import assert_almost_equal
 
 from py_sc_fermi.inputs import volume_from_structure, read_unitcell_data
 from pymatgen.core import Structure
 
-structure = Structure.from_file('/examples/v_F/POSCAR')
+structure = Structure.from_file('inputs/POSCAR')
 volume = Structure.volume
 
 def test_volume_from_structure():
-    assert volume == get_volume_from_structure('/examples/v_F/POSCAR')
+    assert volume == get_volume_from_structure('inputs/POSCAR')
 
 def test_read_unitcell_data():
-    assert volumt == read_unitcell_data('inputs/unitcell.dat')
+    assert_almost_equal(volume,read_unitcell_data('inputs/unitcell.dat'))
     
 if __name__ == '__main__':
     unittest.main()
