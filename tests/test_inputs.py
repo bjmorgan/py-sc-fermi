cdd9db92bf134556cf2a11bb7682dc1a66ca2d17
alexsquires <alex.g.squires@gmail.com>
2022-02-24 01:17:53 +0000

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from py_sc_fermi.inputs import volume_from_structure
+from py_sc_fermi.inputs import volume_from_structure, read_unitcell_data
 from pymatgen.core import Structure
 
 structure = Structure.from_file('/examples/v_F/POSCAR')
@@ -9,5 +9,8 @@ volume = Structure.volume
 def test_volume_from_structure():
     assert volume == get_volume_from_structure('/examples/v_F/POSCAR')
 
+def test_read_unitcell_data():
+    assert volumt == read_unitcell_data('inputs/unitcell.dat')
+    
 if __name__ == '__main__':
     unittest.main()
