4d29e8bedc3c1d71a4002af9e0adde5cfac6f837
alexsquires <alex.g.squires@gmail.com>
2022-04-08 16:34:51 +0100

    sort out docs pathing

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../py-sc-fermi'))
 
 # -- Project information -----------------------------------------------------
 
@@ -27,8 +27,11 @@ author = 'Alex G. Squires, Benjamin J. Morgan'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,9 +47,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,5 @@
-.. py-sc-fermi documentation master file, created by
-   sphinx-quickstart on Fri Apr  8 15:31:07 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
-Welcome to py-sc-fermi's documentation!
+py-sc-fermi
 =======================================
 
 .. toctree::
