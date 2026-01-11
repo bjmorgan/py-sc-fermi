08f381fba817d220829b61ac58cfd508b2a9630c
Benjamin <b.j.morgan@bath.ac.uk>
2026-01-11 11:45:32 +0000

    Fixing formatting

--- py_sc_fermi/warnings.py
+++ py_sc_fermi/warnings.py
@@ -6,17 +6,17 @@ import numpy as np
 
 
 def suppresses_numpy_overflow(func):
-	"""Decorator that suppresses numpy overflow during calculation.
+    """Decorator that suppresses numpy overflow during calculation.
 
-	Overflow commonly occurs during Fermi energy solving when evaluating
-	carrier and defect concentrations at extreme energies. This is expected
-	behaviour and the results (inf) are mathematically correct.
-	"""
-	@wraps(func)
-	def wrapper(*args, **kwargs):
-		old_settings = np.seterr(over='ignore')
-		try:
-			return func(*args, **kwargs)
-		finally:
-			np.seterr(**old_settings)
-	return wrapper
+    Overflow commonly occurs during Fermi energy solving when evaluating
+    carrier and defect concentrations at extreme energies. This is expected
+    behaviour and the results (inf) are mathematically correct.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        old_settings = np.seterr(over='ignore')
+        try:
+            return func(*args, **kwargs)
+        finally:
+            np.seterr(**old_settings)
+    return wrapper
